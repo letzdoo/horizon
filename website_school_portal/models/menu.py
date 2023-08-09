678f24bdc6ef65b1adb9870da098d7fa8436599b
Bastien Vizzini <bv@neodiensis.com>
2023-08-09 11:48:05 +0200

    Modified menus creation for school_portal and crlg_portal

--- website_school_portal/models/menu.py
+++ website_school_portal/models/menu.py
@@ -16,45 +16,59 @@ class Menu(models.Model):
     @api.model
     def init_school_portal_menu(self):
         _logger.info("Init school portal menu")
-        # menus = self.env["website.menu"].search([])
-
+        
         website_id = self.env["website"].get_current_website().id
-        default_main_menu_id = self.env["website.menu"].search([('parent_id', '=', None), ('website_id', '=', website_id)]).id
+        reservation_menus = self.env["website.menu"].search([("url", "=", "/reservations/mes-reservations"), ('website_id', '=', website_id)])
 
-        parent = self.env['website.menu'].create({
-                'name': "Réservations",
-                'url': None,
-                'page_id': None,
-                'parent_id': default_main_menu_id,
-                'website_id': website_id,
-                'public' : False,
-                'student' : True,
-                'teacher' : True,
-                'employee' : True,
-            })
+        # Ajout des points de menu des réservations
+        if len(reservation_menus) == 0:
+            default_main_menu_id = self.env["website.menu"].search([('parent_id', '=', None), ('website_id', '=', website_id)]).id
+
+            parent = self.env['website.menu'].create({
+                    'name': "Réservations",
+                    'url': None,
+                    'page_id': None,
+                    'parent_id': default_main_menu_id,
+                    'website_id': website_id,
+                    'public' : False,
+                    'student' : True,
+                    'teacher' : True,
+                    'employee' : True,
+                })
+            
+            self.env['website.menu'].create({
+                    'name': "Mes réservations",
+                    'url': "/reservations/mes-reservations",
+                    'page_id': None,
+                    'parent_id': parent.id,
+                    'website_id': website_id,
+                    'public' : False,
+                    'student' : True,
+                    'teacher' : True,
+                    'employee' : True,
+                })
+            
+            self.env['website.menu'].create({
+                    'name': "Créer une réservation",
+                    'url': "/reservations/creation",
+                    'page_id': None,
+                    'parent_id': parent.id,
+                    'website_id': website_id,
+                    'public' : False,
+                    'student' : True,
+                    'teacher' : True,
+                    'employee' : True,
+                })
+            
+            self.env.cr.commit()
         
-        self.env['website.menu'].create({
-                'name': "Mes réservations",
-                'url': "/reservations/mes-reservations",
-                'page_id': None,
-                'parent_id': parent.id,
-                'website_id': website_id,
-                'public' : False,
-                'student' : True,
-                'teacher' : True,
-                'employee' : True,
-            })
-        
-        self.env['website.menu'].create({
-                'name': "Créer une réservation",
-                'url': "/reservations/creation",
-                'page_id': None,
-                'parent_id': parent.id,
-                'website_id': website_id,
-                'public' : False,
-                'student' : True,
-                'teacher' : True,
-                'employee' : True,
-            })
-        
+        # Suppression des points de menu Accueil et Contactez-nous
+        self.env['website.menu'].search([('url', '=', '/')]).unlink()
+        self.env['website.menu'].search([('url', '=', '/contactus')]).unlink()
         self.env.cr.commit()
+
+    def contains(self, list, filter):
+        for x in list:
+            if filter(x):
+                return True
+        return False
