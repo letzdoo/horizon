d07d2b7bd920db9423183764da8a4a419e8298e1
Bastien Vizzini <bv@neodiensis.com>
2023-08-11 09:41:22 +0200

    Removed valves pages from sitemap

--- website_crlg_portal/controllers/controllers.py
+++ website_crlg_portal/controllers/controllers.py
@@ -8,11 +8,11 @@ _logger = logging.getLogger(__name__)
 
 class WebCRLGController(http.Controller):
     
-    @http.route(['/etudiants-info'], type='http', auth='user', website=True)
+    @http.route(['/etudiants-info'], type='http', auth='user', website=True, sitemap=False)
     def student_info(self, redirect=None, **post):
         return request.render("website_crlg_portal.valves_etudiants_crlg")
         
-    @http.route(['/professeurs-info'], type='http', auth='user', website=True)
+    @http.route(['/professeurs-info'], type='http', auth='user', website=True, sitemap=False)
     def teacher_info(self, redirect=None, **post):
         if (request.env.user.teacher or request.env.is_admin()):
             return request.render("website_crlg_portal.valves_professeurs_crlg")
