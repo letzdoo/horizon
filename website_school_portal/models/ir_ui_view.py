bed55fecb3fdda672df8da9ccd81df65387676e7
Bastien Vizzini <bv@neodiensis.com>
2023-08-29 12:39:10 +0200

    Added user groups visibility for valves

--- website_school_portal/models/ir_ui_view.py
+++ website_school_portal/models/ir_ui_view.py
@@ -3,6 +3,7 @@ import werkzeug.exceptions
 from odoo import models
 from odoo.http import request
 
+
 class View(models.Model):
 
     _name = "ir.ui.view"
@@ -12,9 +13,14 @@ class View(models.Model):
         try:
             return super(View, self)._handle_visibility(do_raise)
         except werkzeug.exceptions.Forbidden as e:
-            if request.website.is_public_user() and e.description != "website_visibility_password_required":
+            if (
+                request.website.is_public_user()
+                and e.description != "website_visibility_password_required"
+            ):
                 # Si une page n'est pas visible alors que le visiteur n'est pas connecté, on lui propose d'abord de se connecter pour pouvoir juger ensuite de ses droits.
-                redirect = request.redirect_query('/web/login', {'redirect': request.httprequest.full_path}, 302)
+                redirect = request.redirect_query(
+                    "/web/login", {"redirect": request.httprequest.full_path}, 302
+                )
                 werkzeug.exceptions.abort(redirect)
             else:
                 raise
