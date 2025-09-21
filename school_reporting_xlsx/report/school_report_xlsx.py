76eaa31013f29ed80b7981d5a3c1a079d04c9462
Jerome Sonnet (letzdoo) <jerome.sonnet@letzdoo.com>
2025-09-21 15:29:19 +0000

    Add programs export

--- school_reporting_xlsx/report/school_report_xlsx.py
+++ school_reporting_xlsx/report/school_report_xlsx.py
@@ -99,3 +99,51 @@ class RegistrationExportXlsx(models.AbstractModel):
                     worksheet.write(row_num + 1, col_num, json.dumps(col_data, indent=2))
                 else : 
                     worksheet.write(row_num + 1, col_num, col_data)
+
+class ProgramExportXlsx(models.AbstractModel):
+    _name = "report.school_reporting_xlsx.program_export_xlsx"
+    _description = "Report xlsx helpers"
+    _inherit = "report.report_xlsx.abstract"
+    
+    def generate_xlsx_report(self, workbook, data, programs):
+        sheet = workbook.add_worksheet("Programs")
+        # Write titles
+        bold = workbook.add_format({"bold": True})
+        sheet.write(0, 0, "id", bold)
+        sheet.write(0, 1, "name", bold)
+        sheet.write(0, 2, "id_esa", bold)
+        sheet.write(0, 3, "cycle", bold)
+        sheet.write(0, 4, "admission_criteria_url", bold)
+        sheet.write(0, 5, "form_study", bold)
+        sheet.write(0, 6, "generic_grade", bold)
+        sheet.write(0, 7, "Finalité", bold)
+        sheet.write(0, 8, "admission_test_duration", bold)
+        sheet.write(0, 9, "academic_year_id", bold)
+        sheet.write(0, 10, "program_url", bold)
+        sheet.write(0, 11, "Code habilitation", bold)
+        sheet.write(0, 12, "Code Etude ARES", bold)
+        sheet.write(0, 13, "Code grade académique", bold)
+        sheet.write(0, 14, "Année académique de création", bold)
+        sheet.write(0, 15, "Horaire", bold)
+        sheet.write(0, 16, "Published", bold)
+        sheet.write(0, 17, "send_to_hops", bold)
+        sheet.write(0, 18, "Dispositif pédagogique", bold)
+        sheet.write(0, 19, "Soumis au décret R/RN", bold)
+        sheet.write(0, 20, "classification_id", bold)
+        sheet.write(0, 21, "status", bold)
+        sheet.write(0, 22, "Langue", bold)
+        for i, obj in enumerate(programs):
+            sheet.write(i, 0, f'gwr.pedagogical.offer_{obj.id}')
+            sheet.write(i, 1, obj.name)
+            sheet.write(i, 3, "1er cycle" if obj.cycle_id.certification_profile == "bachelor" else "2e cycle")
+            sheet.write(i, 6, f"{obj.cycle_id.name} ({obj.cycle_id.ects_required})")
+            sheet.write(i, 7, obj.speciality_id.name if obj.speciality_id else ""
+            sheet.write(i, 9, obj.year_id.name if obj.academic_year_id else "")
+            sheet.write(i, 11, obj.habilitation_code)
+            sheet.write(i, 12, obj.ares_code)
+            sheet.write(i, 13, obj.graca_code)
+            sheet.write(i, 16, False)
+            sheet.write(i, 17, False)
+            sheet.write(i, 19, False)
+            sheet.write(i, 20, f"/ Art / {obj.speciality_id.domain} / {obj.speciality_id.track_id} / {obj.speciality_id.name}" if obj.speciality_id else "")
+            sheet.write(i, 21, "Actif")
